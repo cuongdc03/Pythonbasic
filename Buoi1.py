7e36759fcb3b365c348ae2c1cba87b1195ae5308
Cao-Cuong Do <88456061+cuongdc03@users.noreply.github.com>
2023-01-25 04:42:08 +0700

    Add files via upload

---
+++ Buoi1.py
@@ -0,0 +1,38 @@
+'''price =10 
+rating=4.9
+name="Cuong"
+is_published= False
+print(price)
+print(rating)
+print(name)
+print(is_published)
+'''
+# Exercise
+"""name="John Smith"
+year_old=20
+patient_status=True
+print(name)
+print(year_old)
+print(patient_status)
+"""
+# Receiving input
+"""name= input('What is your name?')
+print('Hi ' + name)
+
+"""
+# Exercise
+'''name=input('What is your name? ')
+color=input('What is your favourite color? ')
+print(name + ' likes ' + color)'''
+#String
+'''course="Python's course for beginers"
+print(course[1:5])'''
+message=f'Hi,my name is Cuong'
+#print(message.upper())
+#print(message.lower())
+#print(message.find('p')) #return character position in string if not found return -1
+#print(message.replace('p','Cuong Do')) #replace string with another string
+#print(message.title()) #return string with first character in each word is upper case
+# To check a string contains a character or not 
+contains='Cuong' in message
+print(contains)
